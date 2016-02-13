27e6e21dafc32b7e86016ab6d6df906cc788420d
David Langley <david@thewaitlessapp.com>
2016-02-12 17:41:57 -0800

    add css revision control

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="css/compiled/index.css" type="text/css" media="screen" />
 
 	<!-- development overrides -->
-    <link rel="stylesheet" type="text/css" href="css/overrides.css" />
+    <link rel="stylesheet" type="text/css" href="css/overrides.css?id=<?php echo $V; ?>" />
 
     <!-- open sans font -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css' />
