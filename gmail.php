eb26771f19bbbadc6f6a33125153e92a3af590a4
David Langley <david@thewaitlessapp.com>
2016-03-30 22:15:25 -0700

    completed gmail api protocol

--- gmail.php
+++ gmail.php
@@ -1,18 +1,17 @@
 <?php
 	include_once 'inc/dbconnect.php';
 	require_once 'inc/google-api-php-client/src/Google/autoload.php';
-	include_once 'inc/format_email.php';
 	include_once 'phpmailer/PHPMailerAutoload.php';
 
-function sendMessage($service, $userId, $message) {
-  try {
-    $message = $service->users_messages->send($userId, $message);
-    print 'Message with ID: ' . $message->getId() . ' sent.';
-    return $message;
-  } catch (Exception $e) {
-    print 'An error occurred: ' . $e->getMessage();
-  }
-}
+	function sendMessage($service, $userId, $message) {
+		try {
+			$message = $service->users_messages->send($userId, $message);
+			print 'Message with ID: ' . $message->getId() . ' sent.';
+			return $message;
+		} catch (Exception $e) {
+			print 'An error occurred: ' . $e->getMessage();
+		}
+	}
 
 	$query = "SELECT client_secret FROM google; ";
 	$result = qdb($query);
@@ -29,122 +28,38 @@ function sendMessage($service, $userId, $message) {
 
 	// without access token, ask user for permission
 	if ($ACCESS_TOKEN) {
-echo $ACCESS_TOKEN;
+//		echo $ACCESS_TOKEN;
 		$client->setAccessToken($ACCESS_TOKEN);
 
-                $email_message = format_email('test','test body','teaser');
+		//prepare the mail with PHPMailer
+		$mail = new PHPMailer();
+		$mail->CharSet = "UTF-8";
+		$mail->Encoding = "base64";
+		$mail->Priority = 3;
 
-//prepare the mail with PHPMailer
-$mail = new PHPMailer();
-$mail->CharSet = "UTF-8";
-$mail->Encoding = "base64";
+		//supply with your header info, body etc...
+		$mail->Subject = "You've got mail!";
+		$mail->MsgHTML("New test email");
+		$mail->SetFrom("david@ven-tel.com","David Langley");
+		$mail->addAddress("davidglangley@gmail.com");
+		$mail->addBCC("david@ven-tel.com");
 
-//supply with your header info, body etc...
-$mail->Subject = "You've got mail!";
-$mail->Body = "New test email";
+		//create the MIME Message
+		$mail->preSend();
+		$mime = $mail->getSentMIMEMessage();
+		$mime = rtrim(strtr(base64_encode($mime), '+/', '-_'), '=');
 
-//create the MIME Message
-$mail->preSend();
-$mime = $mail->getSentMIMEMessage();
-$mime = rtrim(strtr(base64_encode($mime), '+/', '-_'), '=');
+		//create the Gmail Message
+		$message = new Google_Service_Gmail_Message();
+		$message->setRaw($mime);
 
-//create the Gmail Message
+		$service = new Google_Service_Gmail($client);
 
-$message = new Google_Service_Gmail_Message();
-$message->setRaw($mime);
-$message = $service->users_messages->send('me',$message);
-
-sendMessage($service, 'me', $message);
-
-exit;
+		sendMessage($service, 'me', $message);
 	} else {
 		//$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/mail_auth.php');
 
 		$auth_url = $client->createAuthUrl();
 		header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 	}
-
-
-
-define('APPLICATION_NAME', 'Gmail API PHP Quickstart');
-define('CREDENTIALS_PATH', '~/.credentials/gmail-php-quickstart.json');
-define('CLIENT_SECRET_PATH', __DIR__ . '/json/client_secret.json');
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/gmail-php-quickstart.json
-define('SCOPES', implode(' ', array(
-  Google_Service_Gmail::GMAIL_READONLY)
-));
-
-/**
- * Returns an authorized API client.
- * @return Google_Client the authorized client object
- */
-function getClient() {
-  $client = new Google_Client();
-  $client->setApplicationName(APPLICATION_NAME);
-  $client->setScopes(SCOPES);
-  $client->setAuthConfigFile(CLIENT_SECRET_PATH);
-  $client->setAccessType('offline');
-
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
-  if (file_exists($credentialsPath)) {
-    $accessToken = file_get_contents($credentialsPath);
-  } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
-
-    // Exchange authorization code for an access token.
-    $accessToken = $client->authenticate($authCode);
-
-    // Store the credentials to disk.
-    if(!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
-    }
-    file_put_contents($credentialsPath, $accessToken);
-    printf("Credentials saved to %s\n", $credentialsPath);
-  }
-  $client->setAccessToken($accessToken);
-
-  // Refresh the token if it's expired.
-  if ($client->isAccessTokenExpired()) {
-    $client->refreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, $client->getAccessToken());
-  }
-  return $client;
-}
-
-/**
- * Expands the home directory alias '~' to the full path.
- * @param string $path the path to expand.
- * @return string the expanded path.
- */
-function expandHomeDirectory($path) {
-  $homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv("HOMEDRIVE") . getenv("HOMEPATH");
-  }
-  return str_replace('~', realpath($homeDirectory), $path);
-}
-
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Gmail($client);
-
-// Print the labels in the user's account.
-$user = 'me';
-$results = $service->users_labels->listUsersLabels($user);
-
-if (count($results->getLabels()) == 0) {
-  print "No labels found.\n";
-} else {
-  print "Labels:\n";
-  foreach ($results->getLabels() as $label) {
-    printf("- %s\n", $label->getName());
-  }
-}
-
 ?>
