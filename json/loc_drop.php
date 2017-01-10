fab555fc8b0e21473d5e86261c38aab52103ade2
Aaron <aaron@ven-tel.com>
2017-01-10 17:39:54 +0000

    Json Access to the locations dropdown (in the name of repopulation)

---
+++ json/loc_drop.php
@@ -0,0 +1,15 @@
+<?php 
+        header('Content-Type: application/json');
+        
+        $rootdir = $_SERVER['ROOT_DIR'];
+        include_once $rootdir.'/inc/locations.php';
+        include_once $rootdir.'/inc/form_handle.php';
+        
+        $type = grab('type');
+        $selected = grab('selected');
+        $limit = grab('limit');
+        $warehouse = grab('warehouse');
+        
+        echo json_encode(loc_dropdowns($type, $selcted,$warehouse,$limit));
+    
+?>
