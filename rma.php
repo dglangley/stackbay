cb8bf8179a765d9965c5ba26307f10bd0ff67da6
Aaron <aaron@ven-tel.com>
2017-02-27 22:22:19 +0000

    This should be fine

--- rma.php
+++ rma.php
@@ -27,6 +27,8 @@
 	include_once $rootdir.'/inc/form_handle.php';
 	include_once $rootdir.'/inc/dropPop.php';
 	include_once $rootdir.'/inc/display_part.php';
+	
+	$mode = grab("mode");
 ?>
 
 <!DOCTYPE html>
@@ -63,6 +65,7 @@
 		include_once $rootdir.'/modal/accounts.php';
 		include_once $rootdir.'/modal/alert.php';
 		include_once $rootdir.'/modal/contact.php';
+		
 		?>
 		<div class="row-fluid table-header" id = "order_header" style="width:100%;height:50px;background-color:#f0f4ff;">
 			
@@ -107,7 +110,9 @@
 									<th class="col-md-2">Serials</th>
 									<th class="col-md-2">Disposition</th>
 									<th class="col-md-2">Reason</th>
-									<th class="">History</th>
+									<th class="3">History</th>
+									<th></th>
+									<th></th>
 								</tr>
 							</thead>
 								<tr class="line_item" style = "padding-bottom:6px;">
@@ -119,72 +124,126 @@
 
 										<div class="input-group serial_box">
 										    <input class="form-control input-sm" type="text" name="NewSerial" placeholder="Serial" data-inv-id ="" data-saved="" value='' disabled>
+										    <?php if($mode!="view"){ ?>
 										    <span class="input-group-addon">
 										    	<input type="checkbox" name=""/>
 										    </span>
+										    <? } ?>
 							            </div>
 									<div class="input-group serial_box">
 										    <input class="form-control input-sm" type="text" name="NewSerial" placeholder="Serial" data-inv-id ="" data-saved="" value='' disabled>
+										    <?php if($mode!="view"){ ?>
 										    <span class="input-group-addon">
 										    	<input type="checkbox" name=""/>
 										    </span>
+										    <? } ?>
 							            </div>
 									<div class="input-group serial_box">
 										    <input class="form-control input-sm" type="text" name="NewSerial" placeholder="Serial" data-inv-id ="" data-saved="" value='' disabled>
+										    <?php if($mode!="view"){ ?>
 										    <span class="input-group-addon">
 										    	<input type="checkbox" name=""/>
 										    </span>
+										    <? } ?>
 							            </div>
 
 									</td>
 									<td class="disp-col">
 										<div class='line-disp'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Disposition</div>
+										    <?php }else{?>
 										    <select class="form-control infinite input-sm">
 										    	<option>
 										    		Disposition
 										    	</option>
 										    </select>
+										    <?php }?>
 										</div>
-									<div class='line-disp'>
+										
+										<div class='line-disp'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Disposition</div>
+										    <?php }else{?>
 										    <select class="form-control infinite input-sm">
 										    	<option>
 										    		Disposition
 										    	</option>
 										    </select>
+										    <?php }?>
 										</div>
 										<div class='line-disp'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Disposition</div>
+										    <?php }else{?>
 										    <select class="form-control infinite input-sm">
 										    	<option>
 										    		Disposition
 										    	</option>
 										    </select>
+										    <?php }?>
 										</div>
 									</td>
+
 									<td class="reason-col">
 										<div class='line-reason'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Line Display</div>
+										    <?php }else{?>
 										    <input class="form-control infinite input-sm" type="text" name="partComment" value="" placeholder="Comments" data-serial='' data-inv-id='' data-part="">
+										    <?php }?>
 										</div>
 										<div class='line-reason'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Line Display</div>
+										    <?php }else{?>
 										    <input class="form-control infinite input-sm" type="text" name="partComment" value="" placeholder="Comments" data-serial='' data-inv-id='' data-part="">
+										    <?php }?>
 										</div>
 										<div class='line-reason'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Line Display</div>
+										    <?php }else{?>
 										    <input class="form-control infinite input-sm" type="text" name="partComment" value="" placeholder="Comments" data-serial='' data-inv-id='' data-part="">
+										    <?php }?>
 										</div>										
 										<!--<button class="btn-sm btn-flat pull-right serial-expand" data-serial='serial-<?=$part['id'] ?>' style="margin-top: -40px;"><i class="fa fa-list" aria-hidden="true"></i></button>-->
 									</td>
 									<td class="history-col">
 										<div class = "infinite">
 											Bought 08/02/16 | Sold 08/02/16 | Return 08/05/16 <br>
-											Repaired 08/07/16 | Sold 08/02/16 | <a class="lonk"> Show More</a>
+											Repaired 08/07/16 | Sold 08/07/16 | <a class="lonk"> Show More</a>
 										</div>
 										<div class = "infinite">
 											Bought 08/02/16 | Sold 08/02/16 | Return 08/05/16 <br>
-											Repaired 08/07/16 | Sold 08/02/16 | <a class="lonk"> Show More</a>
+											Repaired 08/07/16 | Sold 08/07/16 | <a class="lonk"> Show More</a>
 										</div>
 										<div class = "infinite">
 											Bought 08/02/16 | Sold 08/02/16 | Return 08/05/16 <br>
+											Repaired 08/07/16 | Sold 08/07/16 | <a class="lonk"> Show More</a>
 										</div>
-											Repaired 08/07/16 | Sold 08/02/16 | <a class="lonk"> Show More</a>
+									</td>
+									<td>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
+										</div>
+									</td>
+									<td>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-trash fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-trash fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-trash fa-4' aria-hidden='true'></i>
+										</div>
 									</td>
 								</tr>
 								<tr class="line_item" style = "padding-bottom:6px;">
@@ -196,71 +255,127 @@
 
 										<div class="input-group serial_box">
 										    <input class="form-control input-sm" type="text" name="NewSerial" placeholder="Serial" data-inv-id ="" data-saved="" value='' disabled>
+										    <?php if($mode!="view"){ ?>
 										    <span class="input-group-addon">
 										    	<input type="checkbox" name=""/>
 										    </span>
+										    <? } ?>
 							            </div>
 									<div class="input-group serial_box">
 										    <input class="form-control input-sm" type="text" name="NewSerial" placeholder="Serial" data-inv-id ="" data-saved="" value='' disabled>
+										    <?php if($mode!="view"){ ?>
 										    <span class="input-group-addon">
 										    	<input type="checkbox" name=""/>
 										    </span>
+										    <? } ?>
 							            </div>
 									<div class="input-group serial_box">
 										    <input class="form-control input-sm" type="text" name="NewSerial" placeholder="Serial" data-inv-id ="" data-saved="" value='' disabled>
+										    <?php if($mode!="view"){ ?>
 										    <span class="input-group-addon">
 										    	<input type="checkbox" name=""/>
 										    </span>
+										    <? } ?>
 							            </div>
 
 									</td>
 									<td class="disp-col">
 										<div class='line-disp'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Disposition</div>
+										    <?php }else{?>
 										    <select class="form-control infinite input-sm">
 										    	<option>
 										    		Disposition
 										    	</option>
 										    </select>
+										    <?php }?>
 										</div>
-									<div class='line-disp'>
+										
+										<div class='line-disp'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Disposition</div>
+										    <?php }else{?>
 										    <select class="form-control infinite input-sm">
 										    	<option>
 										    		Disposition
 										    	</option>
 										    </select>
+										    <?php }?>
 										</div>
 										<div class='line-disp'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Disposition</div>
+										    <?php }else{?>
 										    <select class="form-control infinite input-sm">
 										    	<option>
 										    		Disposition
 										    	</option>
 										    </select>
+										    <?php }?>
 										</div>
 									</td>
 									<td class="reason-col">
 										<div class='line-reason'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Line Display</div>
+										    <?php }else{?>
 										    <input class="form-control infinite input-sm" type="text" name="partComment" value="" placeholder="Comments" data-serial='' data-inv-id='' data-part="">
-										</div>
-									<div class='line-reason'>
-										    <input class="form-control infinite input-sm" type="text" name="partComment" value="" placeholder="Comments" data-serial='' data-inv-id='' data-part="">
+										    <?php }?>
 										</div>
 										<div class='line-reason'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Line Display</div>
+										    <?php }else{?>
 										    <input class="form-control infinite input-sm" type="text" name="partComment" value="" placeholder="Comments" data-serial='' data-inv-id='' data-part="">
+										    <?php }?>
+										</div>
+										<div class='line-reason'>
+										    <?php if($mode=='view'){?>
+										    <div class="infinite" style="line-height:30px;">Line Display</div>
+										    <?php }else{?>
+										    <input class="form-control infinite input-sm" type="text" name="partComment" value="" placeholder="Comments" data-serial='' data-inv-id='' data-part="">
+										    <?php }?>
 										</div>										
 										<!--<button class="btn-sm btn-flat pull-right serial-expand" data-serial='serial-<?=$part['id'] ?>' style="margin-top: -40px;"><i class="fa fa-list" aria-hidden="true"></i></button>-->
 									</td>
 									<td class="history-col">
-										<div class='line-history infinite'>
-											<button class="btn-sm btn-flat"><i class="fa fa-history" aria-hidden="true"></i></button>
+										<div class = "infinite" style="line-height:15px;">
+											Bought 08/02/16 | Sold 08/02/16 | Return 08/05/16 <br>
+											Repaired 08/07/16 | Sold 08/07/16 | <a class="show_history lonk"> Show More</a>
 										</div>
-										<div class='line-history infinite'>
-											<button class="btn-sm btn-flat"><i class="fa fa-history" aria-hidden="true"></i></button>
+										<div class = "infinite">
+											Bought 08/02/16 | Sold 08/02/16 | Return 08/05/16 <br>
+											Repaired 08/07/16 | Sold 08/07/16 | <a class="show_history lonk"> Show More</a>
 										</div>
-										<div class='line-history infinite'>
-											<button class="btn-sm btn-flat"><i class="fa fa-history" aria-hidden="true"></i></button>
+										<div class = "infinite">
+											Bought 08/02/16 | Sold 08/02/16 | Return 08/05/16 <br>
+											Repaired 08/07/16 | Sold 08/07/16 | <a class="show_history lonk"> Show More</a>
 										</div>
 									</td>
-								</tr>								
+									<td>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
+										</div>
+									</td>
+									<td>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-trash fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-trash fa-4' aria-hidden='true'></i>
+										</div>
+										<div class = "infinite" style="line-height:30px;">
+											<i class='fa fa-trash fa-4' aria-hidden='true'></i>
+										</div>
+									</td>
+								</tr>
 						</table>
 					</div>
 				</div>
