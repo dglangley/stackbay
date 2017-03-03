f7fb91915a4aebe45b91b6879c6bc9857dbc3ceb
Andrew Kuan <andrew@ven-tel.com>
2017-03-03 00:39:31 +0000

    Add in RMA Post form changes

--- rma.php
+++ rma.php
@@ -37,11 +37,65 @@
 	$date_created = '';
 	$yesterday = '';
 	
-	
 	//Determine Mode AND if it was referred from a pre-existing Sales Order
 	$mode = grab("mode");
 	$so_number = grab("on");
-	$rma_number = grab("rma");
+	$rma_number = (grab("rma") ? grab("rma") : '');
+	
+	//Check for any post data from save
+	//Array = [counter] = invid
+	$checkedItems = $_POST["inventory"];
+	//Array = [inventoryid] = "String for Reason"
+	$reason = $_POST["reason"];
+	//Array = [inventoryid] = return_id
+	$return = $_POST["return"];
+	
+	$companyid = $_POST['companyid'];
+    $contactid = $_POST['contactid'];
+    
+    $disposition = '0';
+	
+	if(!empty($checkedItems)){
+		$rmaSave;
+		$partid;
+		$rmaQuery;
+		$partidQuery;
+		
+		//New RMA
+		if ($rma_number == ""){
+			
+	        $insert = "INSERT INTO `returns`(`created_by`,`companyid`,`order_number`,`order_type`,`contactid`)
+	        VALUES (".$U['contactid'].",".prep($companyid).",".prep($so_number).",'Sale',".prep($contactid).");";
+	        qdb($insert) OR die();
+	        $rma_number = qid();
+	    	
+	    	//$checkedItems contains all the inventory id
+	        foreach($checkedItems as $invid) {
+	        	$partidQuery = "SELECT partid FROM inventory WHERE id = ".res($invid).";";
+	        	$rmaSave = qdb($partidQuery) or die(qe());
+	        	
+	        	if (mysqli_num_rows($rmaSave)>0) {
+					$rmaSave = mysqli_fetch_assoc($rmaSave);
+					$partid = $rmaSave['partid'];
+				}
+	        	
+	        	$reasonInfo = $reason[$invid];
+	        	
+	        	//echo 'Reason: ' . $reasonInfo . ' RMA: ' . $rma_number . ' invid:' . $invid . ' Partid:' . $partid . "<br>";
+	        	
+	        	$rmaQuery = "
+	                INSERT INTO `return_items`
+	                (`partid`,`inventoryid`, `rma_number`, `line_number`, `reason`, `dispositionid`, `qty`) VALUES 
+	                ($partid ,$invid,".$rma_number.",NULL,'".$reasonInfo."',$disposition,1);
+	            ";
+	            
+	            if ($rmaQuery){
+		            //echo($rmaQuery);
+		            qdb($rmaQuery) OR die();
+		        }
+	        }
+		}
+	}
 
 	// echo"<div style='position:fixed;right:15px;bottom:10px;'>";
 	if ($rma_number){
@@ -157,190 +211,192 @@
 		include_once $rootdir.'/modal/alert.php';
 		include_once $rootdir.'/modal/contact.php';
 		?>
-		<div class="row-fluid table-header" id = "order_header" style="width:100%;height:50px;background-color:#f0f4ff;">
+		
+		<form method="post">
 			
-			<div class="col-md-4">
-				<?php
-                    // Add in the following to link to the appropriate page | href="/'.$url.'.php?on=' . $order_number . '" | href="/docs/'.$order_type[0].'O'.$order_number.'.pdf"
-					echo '<a class="btn-flat pull-left">To Origin</a> ';
-					echo '<a class="btn-flat pull-left" target="_new">Create Printable RMA</a>';
-				?>
+			<div class="row-fluid table-header" id = "order_header" style="width:100%;height:50px;background-color:#f0f4ff;">
 				
-			</div>
-			<div class="col-md-4 text-center"><h2 class = "minimal" style ="margin-top:10px;;">
-			    <?=($rma_number)? "RMA #$rma_number" :'New RMA';?>
-			    </h2>
-			</div>
-		<form  action="rma_submit.php" method="post">
-			<div class="col-md-4">
-				<button class="btn-flat btn-sm  <?=($order_number=="New")?'success':'success'?> pull-right" id = "save_button" data-validation="left-side-main" style="margin-top:2%;margin-bottom:2%;">
-					<?=($order_number=="New") ? 'Create' :'Save'?>
-				</button>
-			</div>
-		</div>
-		
-		<!-- Row declaration for splitting the two "halves of the page  -->
-		<div class="container-fluid">
-			<!--================== Begin Left Half ===================-->
-			<div class="rma-macro col-md-3 col-lg-2" data-page="order" style="height:100%;background-color:#efefef;padding-top:15px;">
+				<div class="col-md-4">
+					<?php
+	                    // Add in the following to link to the appropriate page | href="/'.$url.'.php?on=' . $order_number . '" | href="/docs/'.$order_type[0].'O'.$order_number.'.pdf"
+						echo '<a class="btn-flat pull-left">To Origin</a> ';
+						echo '<a class="btn-flat pull-left" target="_new">Create Printable RMA</a>';
+					?>
+					
+				</div>
+				<div class="col-md-4 text-center"><h2 class = "minimal" style ="margin-top:10px;;">
+				    <?=($rma_number)? "RMA #$rma_number" :'New RMA';?>
+				    </h2>
+				</div>
 				
+				<div class="col-md-4">
+					<button class="btn-flat btn-sm  <?=($order_number=="New")?'success':'success'?> pull-right" id = "rma_save_button" data-validation="left-side-main" style="margin-top:2%;margin-bottom:2%;">
+						<?=($order_number=="New") ? 'Create' :'Save'?>
+					</button>
+				</div>
 			</div>
+			
+			<!-- Row declaration for splitting the two "halves of the page  -->
+			<div class="container-fluid">
+				<!--================== Begin Left Half ===================-->
+				<div class="rma-macro col-md-3 col-lg-2" data-page="order" style="height:100%;background-color:#efefef;padding-top:15px;">
+					
+				</div>
+			
+				<!--======================= End Left half ======================-->
+			
 		
-			<!--======================= End Left half ======================-->
-		
-	
-			<!--===================== Begin Right half =====================-->
+				<!--===================== Begin Right half =====================-->
 				<div class="col-sm-10 shipping-list" style="padding-top: 20px">
 					<div class="table-responsive">
-							<table class="rma-create table table-hover table-striped table-condensed" style="margin-top: 15px;">
-								<thead>
-									<tr>
-										<th class="col-md-3">Item</th>
-										<th class="col-md-2">Serials</th>
-										<th class="col-md-2">Disposition</th>
-										<th class="col-md-2">Reason</th>
-										<th class="">History</th>
-										<th></th>
-										<th></th>
-									</tr>
-								</thead>
-								<?php foreach ($rma_items as $part => $row): ?>
-								<?php $i++;?>
-									<!-- html... -->
-									<tr class="line_item" style = "padding-bottom:6px;">
-										<td class="part"  data-partid="" style="padding-top: 5px !important;vertical-align:top !important;">
-											<?=display_part(current(hecidb($part,"id")));?>
-										</td>
-									<!-- Grab the old serial values from the database and display them-->
-										<td class="serials-col" style="">
-											<?php $initial = $i; ?>
-											<?php $line = $initial;?>
-											<?php foreach ($row as $i => $inf): ?>
-												<?php $i++; ?>
+						<table class="rma-create table table-hover table-striped table-condensed" style="margin-top: 15px;">
+							<thead>
+								<tr>
+									<th class="col-md-3">Item</th>
+									<th class="col-md-2">Serials</th>
+									<th class="col-md-2">Disposition</th>
+									<th class="col-md-2">Reason</th>
+									<th class="">History</th>
+									<th></th>
+									<th></th>
+								</tr>
+							</thead>
+							<?php foreach ($rma_items as $part => $row): ?>
+							<?php $i++;?>
+								<!-- html... -->
+								<tr class="line_item" style = "padding-bottom:6px;">
+									<td class="part"  data-partid="" style="padding-top: 5px !important;vertical-align:top !important;">
+										<?=display_part(current(hecidb($part,"id")));?>
+									</td>
+								<!-- Grab the old serial values from the database and display them-->
+									<td class="serials-col" style="">
+										<?php $initial = $i; ?>
+										<?php $line = $initial;?>
+										<?php foreach ($row as $i => $inf): ?>
+											<?php $i++; ?>
+											
+											<div class="input-group serial_box">
+											    <input class="form-control input-sm" type="text" name="serial_<?=$line?>" placeholder="Serial" data-inv-id ="" value="<?=$inf['serial_no']?>" readonly>
+
+												<!--Array Version-->
+												<input type="text" name="return[<?=$inf['inventoryid']?>]" style="display:none;" value = "<?=$inf['id']?>"/>
 												
-												<div class="input-group serial_box">
-												    <input class="form-control input-sm" type="text" name="serial_<?=$line?>" placeholder="Serial" data-inv-id ="" value="<?=$inf['serial_no']?>" readonly>
-												    <input type="text" name="invid_<?=$line?>" style="display:none;" value = "<?=$inf['inventoryid']?>"/>
-												    <input type="text" name="partid_<?=$line?>" style="display:none;" value = "<?=$part?>"/>
-													<input type="text" name="retid_<?=$line?>" style="display:none;" value = "<?=$inf['id']?>"/>
-												    <?php if($mode!="view"){ ?>
-												    <span class="input-group-addon">
-												    	<input type="checkbox" name="line_<?=$line?>_selected" <?=($inf['id'])? "checked" : ''?>/>
-												    </span>
-												    <?php } ?>
-									            </div>
-												<?php $line++; ?>
-											<?php endforeach; ?>
-	
-										</td>
-										<td class="disp-col">
-											<?php	$line = $initial;?>
-											<?php foreach ($row as $i => $inf):?>
-												<div class='line-disp'>
-												    <?php if($mode=='view'){?>
-												    <div class="infinite" style="line-height:30px;">Disposition</div>
-												    <?php }else{?>
-												    <select class="form-control infinite input-sm" name = "disposition_<?=$line?>">
-												    	<option value = "0">Disposition</option>
-												    </select>
-												    <?php }?>
-												</div>
-												<?php $line++; ?>
-											<?php endforeach; ?>
-										</td>
-	
-										<td class="reason-col">
-											<?php	$line = $initial;?>
-											<?php foreach ($row as $i => $inf):?>
-											
-												<div class='line-reason'>
-												    <?php if($mode=='view'){?>
-												    <div class="infinite" style="line-height:30px;"><?=$inf['reason']?></div>
-												    <?php }else{?>
-												    <input class="form-control infinite input-sm" type="text" name="reason_<?=$line?>" value="<?=$inf['reason']?>" placeholder="Reason" data-row='<?=$inf['id']?>' data-part="">
-												    <?php }?>
-												</div>
-												<?php $line++; ?>
-											<?php endforeach; ?>
-										</td>
-										<td class="history-col">
-											<?php foreach ($row as $i => $inf):?>
-											
-											<div class = "infinite">
-												<?php 
-													$history_text ='';
-													$hist_count = 0;
-													foreach ($inf['history'] as $history){
-														if($hist_count > 0 && $hist_count != 3){echo(" | ");}
-														switch ($history['field']) {
-															case 'sales_item_id':
-																$action = "Sold";
-																break;
-															case 'return_items_id':
-																$action = "Returned";
-																break;
-															case 'purchase_item_id':
-																$action = "Purchased";
-																break;
-																
-														} 
-														 echo  $action.": ".$history['date'];
-														$hist_count++;
-														if ($hist_count == 3){
-															echo  "<br>";
-														}
-														else if ($hist_count == 6){
-															echo "<a class = 'lonk'>Show more</a>";
-															break;
-														}
-													}
-													if ($hist_count < 3){
-														echo"<br>&nbsp;";
-													}
-		
-												?>
-											</div>
-											<?php endforeach; ?>
-											
-										</td>
-										<td>
-											<?php	$line = $initial;?>
-											<?php foreach ($row as $i => $inf):?>
-												<div class = "infinite" style="line-height:30px;">
-													<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
-												</div>
-												<?php $line++; ?>
-											<?php endforeach; ?>
-										</td>
-										<td>
-											<?php	$line = $initial;?>
-											<?php foreach ($row as $i => $inf):?>
-											
-												<div class = "infinite" style="line-height:30px;">
-													<i class='fa fa-trash fa-4' aria-hidden='true'></i>
-												</div>
+											    <?php if($mode!="view"){ ?>
+											    <span class="input-group-addon">
+											    	<input type="checkbox" name="inventory[]" value="<?=$inf['inventoryid']?>" <?=($inf['id'])? "checked" : ''?>/>
+											    </span>
+											    <?php } ?>
+								            </div>
 											<?php $line++; ?>
-											<?php endforeach; ?>
-										</td>
-									</tr>
-								<?php endforeach; ?>
-							</table>
+										<?php endforeach; ?>
+	
+									</td>
+									<td class="disp-col">
+										<?php	$line = $initial;?>
+										<?php foreach ($row as $i => $inf):?>
+											<div class='line-disp'>
+											    <?php if($mode=='view'){?>
+											    <div class="infinite" style="line-height:30px;">Disposition</div>
+											    <?php }else{?>
+											    <select class="form-control infinite input-sm" name = "disposition_<?=$line?>">
+											    	<option value = "0">Disposition</option>
+											    </select>
+											    <?php }?>
+											</div>
+											<?php $line++; ?>
+										<?php endforeach; ?>
+									</td>
+	
+									<td class="reason-col">
+										<?php	$line = $initial;?>
+										<?php foreach ($row as $i => $inf):?>
+										
+											<div class='line-reason'>
+											    <?php if($mode=='view'){?>
+											    <div class="infinite" style="line-height:30px;"><?=$inf['reason']?></div>
+											    <?php }else{?>
+											    <input class="form-control infinite input-sm" type="text" name="reason[<?=$inf['inventoryid']?>]" value="<?=$inf['reason']?>" placeholder="Reason" data-row='<?=$inf['id']?>' data-part="">
+											    <?php }?>
+											</div>
+											<?php $line++; ?>
+										<?php endforeach; ?>
+									</td>
+									<td class="history-col">
+										<?php foreach ($row as $i => $inf):?>
+										
+										<div class = "infinite">
+											<?php 
+												$history_text ='';
+												$hist_count = 0;
+												foreach ($inf['history'] as $history){
+													if($hist_count > 0 && $hist_count != 3){echo(" | ");}
+													switch ($history['field']) {
+														case 'sales_item_id':
+															$action = "Sold";
+															break;
+														case 'return_items_id':
+															$action = "Returned";
+															break;
+														case 'purchase_item_id':
+															$action = "Purchased";
+															break;
+															
+													} 
+													 echo  $action.": ".$history['date'];
+													$hist_count++;
+													if ($hist_count == 3){
+														echo  "<br>";
+													}
+													else if ($hist_count == 6){
+														echo "<a class = 'lonk'>Show more</a>";
+														break;
+													}
+												}
+												if ($hist_count < 3){
+													echo"<br>&nbsp;";
+												}
+	
+											?>
+										</div>
+										<?php endforeach; ?>
+										
+									</td>
+									<td>
+										<?php	$line = $initial;?>
+										<?php foreach ($row as $i => $inf):?>
+											<div class = "infinite" style="line-height:30px;">
+												<i class='fa fa-pencil fa-4' aria-hidden='true'></i>
+											</div>
+											<?php $line++; ?>
+										<?php endforeach; ?>
+									</td>
+									<td>
+										<?php	$line = $initial;?>
+										<?php foreach ($row as $i => $inf):?>
+										
+											<div class = "infinite" style="line-height:30px;">
+												<i class='fa fa-trash fa-4' aria-hidden='true'></i>
+											</div>
+										<?php $line++; ?>
+										<?php endforeach; ?>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						</table>
 							
-							<!-- Macro Form Inputs-->
-							<input type="text" name="rows" style="display:none;" value="<?=$i?>"/>
-							<input type="text" name="rma_number" style="display:none;" value="<?=($rma_number)? $rma_number :'new';?>"/>
-							<input type="text" name="origin_number" style="display:none;" value="<?=$so_number?>"/>
-							<input type="text" name="companyid" style="display:none;" value="<?=$sales_macro['companyid']?>"/>
-							<input type="text" name="contactid" style="display:none;" value="<?=$sales_macro['contactid']?>"/>
-							
-						</form>
+						<!-- Macro Form Inputs-->
+						<input type="text" name="companyid" style="display:none;" value="<?=$sales_macro['companyid']?>"/>
+						<input type="text" name="contactid" style="display:none;" value="<?=$sales_macro['contactid']?>"/>
+
 					</div>
 				</div>
-		<!--====================== End Right half ======================-->
-	</div>
+			<!--====================== End Right half ======================-->
+			</div>
+		</form>
 </div>
-		<?php include_once 'inc/footer.php';?>
-		<script src="js/operations.js?id=<?php if (isset($V)) { echo $V; } ?>"></script>
+
+	<?php include_once 'inc/footer.php';?>
+	<script src="js/operations.js?id=<?php if (isset($V)) { echo $V; } ?>"></script>
 
 
 	</body>
