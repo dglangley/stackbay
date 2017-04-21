a0205a32fa3614217d5f4aff29b8a89dafa5ac21
Aaron <aaron@ven-tel.com>
2017-04-21 20:35:36 +0000

    Finished import_quotes

--- auto/import_quotes.php
+++ auto/import_quotes.php
@@ -17,109 +17,7 @@
     include_once $rootdir.'/inc/import_aid.php';
     include_once $rootdir.'/inc/logSearchMeta.php';
 
-    function import_quotes($type = 'outgoing'){
-    //Expects Quotes/Supply
-    //Query to grab the incoming_quote
-    echo("REMEMBER TO SET PRICES TO NULL BEFORE RUNNING ON THE DB");
-    $query = "";
-    if($type == 'outgoing'){
-        $query = "SELECT oq.company_id as company, oq.date, oq.threshold, oq.creator_id, oq.inventory_id invid, 
-        oq.price as quote_price,oq.quantity as quote_quantity , 
-        ur.quantity as req_qty, ur.price as req_price, line_number, oq.notes, sales_rep_id
-        FROM inventory_outgoing_quote oq, inventory_userrequest ur, inventory_quote q
-        WHERE  oq.id = ur.oq_id AND oq.quote_id = q.id;";
-    } else {
-        $query = "SELECT company_id as company, date, threshold, notes, creator_id, inventory_id invid, 
-        price as offer_price, quantity as offer_qty, null AS line_number, null as sales_rep_id
-        FROM inventory_incoming_quote iq;";
-    }
-    $results = qdb($query,"PIPE") or die(qe("PIPE")." ".$query);
-    
-    foreach($results as $row){
-        print_r($row);
-        $ins = array();
-        // get_coldata(); ??
-        //Meta information from inventory_outgoing_quote: 
-        //company_id -> companyid
-        $ins['company'] = $row['company'];
-        
-        //date -> datetime
-        // append datetime as noon
-        if($row['date'] == format_date('n/j/y',$row['date'])){
-            $ins['datetime'] = $row['date']." 12:00:00";
-        } else {
-            $ins['datetime'] = $row['date'];
-        }
-
-        //creator_id -> userid (Through the maps)
-        $ins['userid'] = mapUser($row['creator_id']);
-        if($row['sales_rep_id'] && ($row['sales_rep_id'] != $row['creator_id'])){
-            $ins['userid'] = $row['sales_rep_id'];
-        }
-        
-        $ins['meta_id'] = logSearchMeta($ins['company'], false, $ins['date'],'import');
-        
-        //Demand information from `inventory_outgoing_quote`:
-        //inventory_id -> translateID() -> partid
-        $ins['partid'] = translateID($row['invid']);
-        //price -> quote_price
-        $ins['quote_price'] = $row['quote_price'];
-        //quantity -> quote_qty
-        $ins['quote_qty'] = $row['quote_qty'];
-        //line_number -> line_number
-        $ins['line_number'] = $row['line_number'];
-        
-        
-        //Demand information from joined `inventory_userrequest`:
-        //price -> request_price
-        //quantity -> request_qty
-        $ins['request_qty'] = $row['req_quantity'];
-        $ins['request_price'] = $row['req_price'];    
-        
-        $ins['offer_price'] = $row['offer_price'];
-        $ins['offer_qty'] = $row['offer_qty']; 
-        
-            
-        //========================= INSERT STATEMENT ====================
-        
-        if($type == 'outgoing'){
-            $table = 'demand';
-        } else {
-            $table = 'availability';   
-        }
-        //INCOMING GOES TO AVAILABILITY
-        
-        //Prepped insert query into the `supply/availability` table
-        if($ins['partid']){
-            if($type == 'outgoing'){
-                $insert = "INSERT INTO `demand` 
-                (`partid`, `request_qty`, `request_price`, `quote_qty`, `quote_price`, `metaid`, `line_number`) 
-                VALUES (
-                ".prep($ins['partid']).",
-                ".prep($ins['request_qty']).",
-                ".prep($ins['request_price']).",
-                ".prep($ins['quote_qty']).",
-                ".prep($ins['quote_price']).",
-                ".prep($ins['meta_id']).",
-                ".prep($ins['line_number'])."
-                );";
-            } else {
-                $insert = "INSERT INTO `availability`
-                (`partid`, `avail_qty`, `avail_price`, `offer_qty`, `offer_price`, `metaid` `line_number`)
-                VALUES (
-                ".prep($ins['partid']).",
-                ".prep($ins['avail_qty']).",
-                ".prep($ins['avail_price']).",
-                ".prep($ins['offer_qty']).",
-                ".prep($ins['offer_price']).",
-                ".prep($ins['meta_id']).",
-                ".'null'.",
-                ".prep($ins['line_number'])."
-                );";
-            }
-            qdb($insert) or die(qe());
-        }
-        
+    function threshold_process($row){
         if($row['notes'] || intval($row['threshold']) > 0){
             $note_insert = "";
             if(intval($row['threshold']) > 0){
@@ -129,13 +27,135 @@
                 $note_insert .= $row['notes'];
             }
             $note_insert = prep(trim($note_insert));
-            $prices_insert = "INSERT INTO `prices`(`partid`, `price`, `datetime`, `note`, `userid`) 
-            VALUES (".$ins['partid'].",'-99.99',".prep($ins['datetime']).",".$note_insert.","."null".")";
-                //If threshdold is greater than zero OR notes
-                //take partid, leave price null, set date_time and note: Threshold . $note, user_id, id
         }
+        return $note_insert;
     }
+
+    function import_quotes($type = 'outgoing'){
+        //Expects Quotes/Supply
+        //Query to grab the incoming_quote
+        echo("REMEMBER TO SET PRICES TO NULL BEFORE RUNNING ON THE DB");
+        $query = "";
+        $incoming_fix = "";
+        
+        if($type == 'outgoing'){
+            $incoming_fix = "";
+            $table = "outgoing";
+            $incoming_flag = "0";
+            $ur_ignore = "oq_id";
+            $in_table = "demand";
+            $price_field = '`quote_price`';
+            $qtyprice = " `request_qty`, `quote_price`";
+        } else {
+            $incoming_fix = "null AS";
+            $table = "incoming";
+            $incoming_flag = "1";
+            $ur_ignore = "iq_id";
+            $in_table = "availability";
+            $price_field = '`avail_price`';
+            $qtyprice = " `avail_qty`, `avail_price`";
+        }
+        $query = "
+        SELECT company_id as company, date, q.threshold, creator_id as user, inventory_id invid, 
+        price, quantity, $incoming_fix line_number, q.notes, i.part_number, i.heci, i.clei, i.short_description, im.name manf
+        FROM inventory_".$table."_quote q, inventory_inventory i, inventory_manufacturer im 
+        WHERE q.inventory_id = i.id  AND i.manufacturer_id_id = im.id
+        UNION
+        SELECT company_id as company, date, null AS threshold, user_id as user, inventory_id invid, 
+        price, quantity, null AS line_number, ur.notes, i.part_number, i.heci, i.clei, i.short_description, im.name manf
+        FROM inventory_userrequest ur, inventory_inventory i, inventory_manufacturer im 
+        WHERE incoming = '$incoming_flag' AND $ur_ignore IS NULL AND ur.inventory_id = i.id  AND i.manufacturer_id_id = im.id
+        ;";
+        
+        
+        $results = qdb($query,"PIPE") or die(qe("PIPE")." ".$query);
+    
+        $quotes = array();
+        foreach($results as $row){
+            $notes = "";
+            //partid is occasionally zero, please advise
+            $partid = part_process($row);
+            if(!$partid) {
+                echo("Partid is zero")."<br>";
+                print_r($row);
+                echo("<br><br>");
+            }
+            if(!(isset($quotes[$row['date']][$row['company']][$row['user']][$partid][$row['price']]))){
+                $notes = threshold_process($row);
+                $quotes[$row['date']][$row['company']][$row['user']][$partid][$row['price']] = array(
+                    "qty" => $row['quantity'],
+                    "ln" => ($row['line_number']? $row['line_number'] : null),
+                    "notes" => $notes
+                    );
+            } else if($quotes[$row['date']][$row['company']][$row['user']][$partid][$row['price']]['qty'] < $row['quantity']){
+                $quotes[$row['date']][$row['company']][$row['user']][$partid][$row['price']]['qty'] = $row['quantity'];
+            }
+        } 
+            // echo("<br><pre>");
+            // print_r($quotes);
+            // echo("</pre><br>");
+            // exit;
+    
+        
+        
+    //Part Processing
+    foreach($quotes as $date => $company){
+        $in = array();
+        if($row['date'] == format_date('n/j/y',$row['date'])){
+                $in['datetime'] = $date." 12:00:00";
+            } else {
+                $in['datetime'] = $date;
+            }
+        foreach($company as $companyid => $user){
+            $in['companyid'] = dbTranslate($companyid);
+            
+            foreach($user as $userid => $part){
+                $in['userid'] = mapUser($userid);
+                ////LOG SEARCH META DOES NOT CALL A USERID FUNCTION TO DISTINGUISH////
+                $meta_id = logSearchMeta($in['companyid'], false, $in['datetime'], 'import', $in['userid']);
+                foreach($part as $partid => $price_group){
+                    foreach($price_group as $price => $row){
+                        if ($price == 0 || $price == 0.00 || $price == 0.0000 || $price == null){
+                            $in['price'] = 0.00;
+                        } else {
+                            $in['price'] = $price;
+                        }
+                        $check_unique_row = "
+                        SELECT count(*) as rows FROM `$in_table`
+                        WHERE `metaid` = $meta_id
+                        AND $price_field = ".$in['price']."
+                        AND `partid` = ".prep($partid)."
+                        ;";
+                        $unique = qdb($check_unique_row) or die(qe()." | $check_unique_row");
+                        $count = mysqli_fetch_assoc($unique);
+                        if($count['rows'] == 0){
+                            $insert = "INSERT INTO `$in_table` (`partid`, $qtyprice, `metaid`, `line_number`) 
+                                VALUES (
+                                ".prep($partid).",
+                                ".prep($row['qty']).",
+                                ".prep($price).",
+                                ".prep($meta_id).",
+                                ".prep($row['ln'])."
+                                );";
+                            qdb($insert) or die(qe()." ".$insert);
+                                
+                            if ($row['notes']){
+                                $prices_insert = "INSERT INTO `prices`(`partid`, `price`, `datetime`, `note`, `userid`) 
+                                VALUES (".$partid.",'-99.99',".prep($in['datetime']).",".$row['notes'].","."null".")";
+                                qdb($prices_insert) or die(qe()." ".$prices_insert);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+    exit('Import Complete');
+
 }
+
     import_quotes();
 ?>
 
