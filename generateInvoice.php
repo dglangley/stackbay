8bc51a7ae598bd9a7d47320498c4d31b2f2d432d
David Langley <david@thewaitlessapp.com>
2017-05-10 15:31:13 -0700

    fixed no results showing on New PO search

--- generateInvoice.php
+++ generateInvoice.php
@@ -2,5 +2,5 @@
 	include_once $_SERVER["ROOT_DIR"].'/inc/dbconnect.php';
 	include_once $_SERVER["ROOT_DIR"].'/inc/invoice.php';
 
-//	create_invoice(20103, "2017-05-10 14:43:58", "Sale");
+//	echo create_invoice(20103, "2017-05-10 14:43:58", "Sale");
 ?>
