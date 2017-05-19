e61dc8d0fbca41cd24d620efcb98867243cbd102
David Langley <david@thewaitlessapp.com>
2017-05-19 16:20:36 -0700

    minor php warnings fixes

--- inc/operations_sidebar.php
+++ inc/operations_sidebar.php
@@ -31,7 +31,9 @@ include_once $rootdir.'/inc/packages.php';
 	$rtv_items = array();
 	$rtv_array = array();
 	
-	if(strtolower($_REQUEST['ps']) == 'rtv'){
+	$ps = '';
+	if (isset($_REQUEST['ps'])) { $ps = strtolower($_REQUEST['ps']); }
+	if($ps == 'rtv'){
 		$order_type = "RTV";
 		$origin = $order_number;
 		$order_number = "New";
