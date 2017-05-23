b67eeee42ef683a94cdbb11ab98b443a436ba3ff
Aaron <aaron@ven-tel.com>
2017-05-23 22:53:23 +0000

    mostly repair_add changes

--- repair_add.php
+++ repair_add.php
@@ -51,7 +51,7 @@
 	function getRepairParts ($order_number) {		
 		$listPartid;
 		//Only looking for how many parts are in the RMA, distinct as we will retrieve all the serial pertaining to the part later
-		$query = "SELECT id, partid FROM repair_items WHERE ro_number = ".prep($order_number)." GROUP BY partid;";
+		$query = "SELECT id, partid, notes FROM repair_items WHERE ro_number = ".prep($order_number)." GROUP BY partid;";
 		$result = qdb($query) or die(qe()." $query");
 	    
 	    if($result)
@@ -66,16 +66,16 @@
 	
 
 	//This grabs the return specific items based on the rma_number and partid (Used to grab inventoryid for the same part only)
-	function getRepairItems($partid, $order_number) {
+	function getRepairItems($line_id) {
 
-		$query = "SELECT DISTINCT i.serial_no, i.locationid, r.reason, i.returns_item_id, r.inventoryid, r.dispositionid 
-		FROM return_items  as r, inventory as i 
-		WHERE r.partid = ". res($partid) ." 
-		AND i.id = r.inventoryid 
-		AND r.rma_number = ".res($order_number).";";
-		$result = qdb($query);
-	    
-	    if($result)
+		$query = "
+		SELECT i.serial_no, i.locationid, i.repair_item_id, i.conditionid, i.notes, i.id
+		FROM inventory i 
+		WHERE id in (
+			SELECT DISTINCT invid FROM inventory_history WHERE field_changed = 'repair_item_id' AND value = ".prep($line_id)."
+		);";
+		$result = qdb($query) or die(qe()." | $query");
+		
 	    if (mysqli_num_rows($result)>0) {
 			while ($row = $result->fetch_assoc()) {
 				$listSerial[] = $row;
@@ -85,16 +85,46 @@
 		return $listSerial;
 	}
 	
-	if (grab("form_submitted")){
+if (grab("form_submitted")){
+		
 		$place = grab("place");
 		$instance = grab("instance");
-		$condition = grab("condition");
-		echo("$place | $instance | $condition | ");
-		exit;
+		$condition = prep(grab("condition"));
+		$serial = grab("serial_number");
+		$rlineid = prep(grab("line_id"));
+		
+		if($serial){
+			$serial = prep($serial);
+			$line_item_sel = "SELECT * FROM repair_items where id = $rlineid;";
+			$result = qdb($line_item_sel) or die(qe()." $line_item_sel");
+			$row = mysqli_fetch_assoc($result);
+			$partid = prep($row['partid']);
+			
+			$location_id = prep(dropdown_processor($place, $instance));
+			
+			$quick_check = "SELECT * FROM inventory where serial_no like $serial AND repair_item_id = $rlineid;";
+			$res = qdb($quick_check) or die(qe()." $quick_check");
+			if(!mysqli_num_rows($res)){
+				$insert = "INSERT INTO `inventory`(`serial_no`, `qty`, `partid`, 
+				`conditionid`, `status`, `locationid`, `repair_item_id`, `userid`, `date_created`, `notes`) 
+				VALUES ($serial,1,$partid,$condition,'in repair',$location_id,$rlineid,".$GLOBALS['U']['id'].",NOW(),NULL)";
+				
+				qdb($insert) or die(qe()." $insert");
+				echo(qid());
+			} else {
+				echo"ALREADY SCANNED THIS PART FOR THIS RECORD";
+			}
+		}
+		foreach($_REQUEST['notes'] as $invid => $note){
+			$update = "UPDATE inventory SET `notes` = ".prep($note)." WHERE id = $invid;";
+			qdb($update) or die(qe()." $update");
+		}
 	}
 	
 	$partsListing = getRepairParts($order_number);
 ?>
+
+
 
 <!DOCTYPE html>
 <html>
@@ -192,11 +222,12 @@
 		
 			<!-------------------- $$ OUTPUT THE MACRO INFORMATION -------------------->
 			<div class="col-md-2 rma_sidebar" data-page="addition" style="padding-top: 15px;">
-				<div class="row">
-					<div class="col-sm-12" style="padding-bottom: 10px;font-size:14pt; ">						
-						Repair Ticket #12451
-					</div>
-				</div>
+				<?=sidebar_out($order_number,$o['type'],$o['edit_mode'])?>
+				<!--<div class="row">-->
+				<!--	<div class="col-sm-12" style="padding-bottom: 10px;font-size:14pt; ">						-->
+				<!--		Repair Ticket #12451-->
+				<!--	</div>-->
+				<!--</div>-->
 			</div>
 			
 			<div class="col-sm-10">
@@ -207,21 +238,20 @@
 								<div class="row">
 									<div class="col-md-4" style="padding-left: 0px !important;">
 										<?=loc_dropdowns('place', $itemLocation)?>
-										<?=$locationid;?>
 									</div>
 									
-									<div class="col-md-4">
+									<div class="col-md-3">
 										<?=loc_dropdowns('instance', $itemLocation)?>
 									</div>
 
-									<div class="col-md-4">
+									<div class="col-md-5">
 										<?=dropdown('conditionid', '', '', '',false)?>
 									</div>
 								</div>
 							</div>
 							
 							<div class="col-md-5" style="padding: 0 0 0 5px;">
-							    <input class="form-control input-sm serialInput auto-focus" name="repairid" type="text" placeholder="Serial" value="<?=($rma_serial ? $rma_serial : '');?>" autofocus>
+							    <input class="form-control input-sm serialInput auto-focus" name="serial_number" type="text" placeholder="Serial" value="<?=($rma_serial ? $rma_serial : '');?>" autofocus>
 				            </div>
 				            <div class="col-md-1" style="padding: 0 0 0 5px;">
 								<button type='submit'>submit</button>
@@ -229,112 +259,123 @@
 						    <input class="form-control input-sm serialInput" style='display:none' name="form_submitted" type="text" value="true" autofocus>
 			            </div>
 					</div>
-				</form>
 			
-				<div class="table-responsive">
-					<table class="rma_add table table-hover table-striped table-condensed" style="table-layout:fixed;"  id="items_table">
-						<thead>
-					         <tr>
-					            <th class="col-sm-3">
-					            	PART	
-					            </th>
-					            <th class="text-center col-sm-1">
-									Serial
-					        	</th>
-					        	<th class="col-sm-3">
-									Notes
-					        	</th>
-					        	<th class="text-center col-sm-2">
-									Location
-					        	</th>
-					        	<!--<th class="text-right col-sm-1">-->
-					        	<!--	Receive-->
-					        	<!--</th>-->
-					         </tr>
-						</thead>
-						
-						<tbody>
-						<?php 
-							//Grab all the parts from the specified PO #
-							if(!empty($partsListing)) {
-								foreach($partsListing as $part): 
-									$serials = getRepairItems($part['partid'],$order_number);
-
-						?>
-								<tr>
-
-									<td>
-										<input type="radio" name="part" class="pull-left" style="margin-right: 10px;margin-top:12px;">
-										<div class="product-img pull-left"><img class="img" src="/img/parts/<?php echo $part; ?>.jpg" alt="pic"></div>
-										<div>
-										<?=display_part(current(hecidb($part['partid'],'id')));?>
-										</div>
-									</td>
-									<td class="serials_col">
-										<?php 
-											if(!empty($serials)):
-												foreach($serials as $item) { 
-										?>
-												<div class="row">
-													<!--<div class="input-group">-->
-														<span class="text-center" style="display: block; padding: 7px 0; margin-bottom: 5px;"><?=$item['serial_no'];?></span>
-														<!--<span class="input-group-addon">-->
-													
-														<!--</span>-->
-													<!--</div>-->
-												</div>
-										<?php 
-												} 
-											endif;
-										?>
-									</td>
-									<td class="notes_col">
-										<?php 
-											if(!empty($serials)):
-												foreach($serials as $item) { 
-										?>
-												<div class="row">
-													<!--<div class="input-group">-->
-														<span class="text-center" style="display: block; padding: 7px 0; margin-bottom: 5px;"><?=$item['serial_no'];?></span>
-														<!--<span class="input-group-addon">-->
-													
-														<!--</span>-->
-													<!--</div>-->
-												</div>
-										<?php 
-												} 
-											endif;
-										?>
-									</td>
-									<td class="location_col">
-										<?php 
-											if(!empty($serials)):
-												foreach($serials as $item) { 
-										?>
-												<div class="row">
-													<!--<div class="input-group">-->
-														<span class="text-center" style="display: block; padding: 7px 0; margin-bottom: 5px;"><?=$item['serial_no'];?></span>
-														<!--<span class="input-group-addon">-->
-													
-														<!--</span>-->
-													<!--</div>-->
-												</div>
-										<?php 
-												} 
-											endif;
-										?>
-									</td>
-									<td>
-
-								</tr>
-								
+					<div class="table-responsive">
+						<table class="rma_add table table-hover table-striped table-condensed" style="table-layout:fixed;"  id="items_table">
+							<thead>
+						         <tr>
+						            <th class="col-sm-3">
+						            	PART	
+						            </th>
+						            <th class="text-center col-sm-1">
+										Serial
+						        	</th>
+						        	<th class="text-center col-sm-2">
+										Location
+						        	</th>
+						        	<th class="text-center col-sm-1">
+						        		Condition
+						        	</th>
+						        	<th class="col-sm-3">
+										Notes
+						        	</th>
+						         </tr>
+							</thead>
+							
+							<tbody>
 							<?php 
-									endforeach;
-								} 
+								//Grab all the parts from the specified PO #
+								if(!empty($partsListing)) {
+									$results = count($partsListing);
+									foreach($partsListing as $part): 
+										$serials = getRepairItems($part['id']);
+	
 							?>
-						</tbody>
-					</table>
-				</div>
+									<tr>
+	
+										<td>
+											<input type="radio" name="line_id" class="pull-left" style="margin-right: 10px;margin-top:12px;" value = '<?=$part['id']?>' <?=(($results == 1)? 'checked' : "")?>>
+											<div class="product-img pull-left"><img class="img" src="/img/parts/<?php echo $part; ?>.jpg" alt="pic"></div>
+											<div>
+											<?=display_part(current(hecidb($part['partid'],'id')));?>
+											</div>
+											<div class="meta_notes">
+												<?=$part['notes']?>
+											</div>
+										</td>
+										<td class="serials_col">
+											<?php 
+												if(!empty($serials)):
+													foreach($serials as $item) { 
+											?>
+													<div class="row">
+															<span class="text-center" style="display: block; padding: 7px 0; margin-bottom:4px;"><?=$item['serial_no'];?></span>
+													</div>
+											<?php 
+													} 
+												endif;
+											?>
+										</td>
+										<td class="location_col">
+											<?php 
+												if(!empty($serials)):
+													foreach($serials as $item) { 
+											?>
+													<div class="row">
+														<!--<div class="input-group">-->
+															<span class="text-center" style="display: block; padding: 7px 0; margin-bottom: 4px;"><?=display_location($item['locationid']);?></span>
+															<!--<span class="input-group-addon">-->
+														
+															<!--</span>-->
+														<!--</div>-->
+													</div>
+											<?php 
+													} 
+												endif;
+											?>
+										</td>
+										<td>
+											<?php 
+												if(!empty($serials)):
+													foreach($serials as $item) { 
+											?>
+													<div class="row">
+														<!--<div class="input-group">-->
+															<span class="text-center" style="display: block; padding: 7px 0; margin-bottom: 4px;"><?=getCondition($item['conditionid'])?></span>
+															<!--<span class="input-group-addon">-->
+														
+															<!--</span>-->
+														<!--</div>-->
+													</div>
+											<?php 
+													} 
+												endif;
+											?>	
+										</td>
+										<td class="notes_col">
+											<?php 
+												if(!empty($serials)):
+													foreach($serials as $item) { 
+											?>
+													<div class="row">
+														<input type="text" class = 'form-control' name="notes['<?=$item['id']?>']" value = "<?=$item['notes']?>"/>
+													</div>
+											<?php 
+													} 
+												endif;
+											?>
+										</td>
+	
+									</tr>
+									
+								<?php 
+										endforeach;
+									} 
+								?>
+							</tbody>
+						</table>
+					</div>
+				</form>
 			</div>
 		</div> 
 		
