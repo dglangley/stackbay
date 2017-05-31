7074a0de3b28d76cb636f3cc98a91d973e0b38cf
Andrew Kuan <andrew@ven-tel.com>
2017-05-31 14:01:10 -0700

    Push up the repairs view and Ops Dash Changes

--- repair_activities.php
+++ repair_activities.php
@@ -4,26 +4,28 @@
 	include_once $_SERVER["ROOT_DIR"].'/inc/format_date.php';
 	include_once $_SERVER["ROOT_DIR"].'/inc/form_handle.php';
 
-	function triggerActivity($ro_number, $repair_item, $notes, $techid, $now){
-		$query = "INSERT INTO repairs_activities (ro_number, repair_item_id, datetime, techid, notes) VALUES (".prep($ro_number).", ".prep($repair_item).", ".prep($now).", ".prep($techid).", ".prep($notes).");";
+	function triggerActivity($ro_number, $repair_item_id, $notes, $techid, $now){
+		$query = "INSERT INTO repairs_activities (ro_number, repair_item_id, datetime, techid, notes) VALUES (".prep($ro_number).", ".prep($repair_item_id).", ".prep($now).", ".prep($techid).", ".prep($notes).");";
 		$result = qdb($query) OR die(qe());
 	}
 	
 	//Declare variables
 	$ro_number;
-	$repair_item;
+	$repair_item_id;
 	$notes;
 	$techid;
 	$partid;
 	
 	if (isset($_REQUEST['ro_number'])) { $ro_number = $_REQUEST['ro_number']; }
-	if (isset($_REQUEST['repair_item'])) { $repair_item = $_REQUEST['repair_item']; }
+	if (isset($_REQUEST['repair_item_id'])) { $repair_item_id = $_REQUEST['repair_item_id']; }
 	if (isset($_REQUEST['notes'])) { $notes = $_REQUEST['notes']; }
 	if (isset($_REQUEST['techid'])) { $techid = $_REQUEST['techid']; }
 	if (isset($_REQUEST['partid'])) { $partid = $_REQUEST['partid']; }
 
 	if (isset($_REQUEST['type'])) { 
-		if($_REQUEST['type'] == 'check_in'){
+		if($_REQUEST['type'] == 'claim'){
+			$notes = "Claimed Ticket";
+		} else if($_REQUEST['type'] == 'check_in'){
 			$notes = "Checked In";
 		} else if($_REQUEST['type'] == 'check_out'){
 			$notes = "Checked Out";
@@ -32,7 +34,7 @@
 		}
 	}
 
-	triggerActivity($ro_number, $repair_item, $notes, $techid, $now);
+	triggerActivity($ro_number, $repair_item_id, $notes, $techid, $now);
 	
 	header('Location: /repair.php?on=' . $ro_number);
 
