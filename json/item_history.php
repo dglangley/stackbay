ce2004881c5cbf5b5baa2c32faf2eb4f7e1e2e5b
Aaron <aaron@ven-tel.com>
2017-06-14 19:31:26 +0000

    Hopefully this fixes everything

--- json/item_history.php
+++ json/item_history.php
@@ -103,6 +103,7 @@
 						break;
 
 					case 'new':
+						continue(2);
 						$string = "Item entered into system ";
 						break;
 					default:
@@ -114,7 +115,7 @@
 				// 	$string .= " on <strong>".format_date($r['date_changed'], 'n/d/y')."</strong>";
 				// }
 				if($r['userid']){
-					$string .= " by ".getContact($r['userid']);
+					$string .= " by ".getRep($r['userid']);
 				}
 				$output[(++$i).'. <strong>'.format_date($r['date_changed'],'D n/d/y g:ia').'</strong>'] = ucwords($string);
 	    	}
