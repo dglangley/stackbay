a564d5e70d31e6bb4228736230e832d474cb92c2
David Langley <david@thewaitlessapp.com>
2017-06-27 22:52:06 -0700

    completed integration of cogs for all shipping occasions except rtv

--- json/shipping-update.php
+++ json/shipping-update.php
@@ -56,83 +56,138 @@ $rootdir = $_SERVER['ROOT_DIR'];
 						while ($r2 = mysqli_fetch_assoc($result2)) {
 							$partid = $r2['partid'];
 
-							if (($r2['ref_1'] AND $r2['ref_1_label']=='repair_item_id') OR ($r2['ref_2'] AND $r2['ref_2_label']=='repair_item_id'])) {
+							/***** GET THE REPAIR ITEM ID AND FIND OUT WHAT TYPE OF REPAIR IT IS *****/
+							if (($r2['ref_1'] AND $r2['ref_1_label']=='repair_item_id') OR ($r2['ref_2'] AND $r2['ref_2_label']=='repair_item_id')) {
 								/***** REPAIR (RMA or BILLABLE) *****/
 								$repair_item_id = 0;
-								if ($r2['ref_1'] AND $r2['ref_1_label']=='repair_item_id']) {
+								if ($r2['ref_1'] AND $r2['ref_1_label']=='repair_item_id') {
 									$repair_item_id = $r2['ref_1'];
 								} else {
 									$repair_item_id = $r2['ref_2'];
 								}
+
+								// find out if the repair is a billable repair order, or a warranty replacement. if a billable repair,
+								// the freight gets charged to the customer as an expense, so not claimable as COGS by freight. however, if
+								// a warranty repair, then the freight gets added back to the original billable order as COGS because
+								// we can't charge the customer, and it's an incurred cost to that original billable sale/repair
 								$ro_number = 0;
 								$query3 = "SELECT ro_number FROM repair_items WHERE id = '".$repair_item_id."'; ";
 								$result3 = qdb($query3) OR die(qe().'<BR>'.$query3);
-								if (mysqli_num_rows($result3)>0) {
-									$r3 = mysqli_fetch_assoc($result3);
-									$ro_number = $r3['ro_number'];
+								if (mysqli_num_rows($result3)==0) {
+									// really should never be a case this doesn't come up, but just the same...escape
+									continue;
+								}
 
-									// find out if the repair is a billable repair order, or a warranty replacement. if a billable repair,
-									// the freight gets charged to the customer as an expense, so not claimable as COGS by freight. however, if
-									// a warranty repair, then the freight gets added back to the original billable order as COGS because
-									// we can't charge the customer, and it's an incurred cost to that original billable sale/repair
-									$NONBILLABLE = true;
-									$query4 = "SELECT termsid FROM repair_orders ro WHERE ro.ro_number = '".res($ro_number)."'; ";
-									$result4 = qdb($query4) OR die(qe().'<BR>'.$query4);
-									// with an existing record, check the terms id for anything other than "N/A" (non-billable)
-									if (mysqli_num_rows($result4)>0) {
-										$r4 = mysqli_fetch_assoc($result4);
-										if ($r4['termsid']<>15) {//15==N/A
-											$NONBILLABLE = false;
-										}
-									}
+								$r3 = mysqli_fetch_assoc($result3);
+								$ro_number = $r3['ro_number'];
 
-									// get cost of repair order for this unit
-									$cogs = calcRepairCost($ro_number,$repair_item_id,$inventoryid,$NONBILLABLE);//get repair cost
-
-									// If non-billable, the $cogs should be added to the *originating* repair, not this most direct
-									// repair, since this is the warranty repair for that original billable repair
-									if ($NONBILLABLE) {
-										$query4 = "SELECT ref_1, ref_1_label, ref_2, ref_2_label FROM repair_items ";
-										$query4 .= "WHERE id = '".$repair_item_id."' AND (ref_1_label = 'return_item_id' OR ref_2_label = 'return_item_id'); ";
-										$result4 = qdb($query4) OR die(qe().'<BR>'.$query4);
-										if (mysqli_num_rows($result4)>0) {
-											$r4 = mysqli_fetch_assoc($result4);
-											$return_item_id = 0;
-											if ($r4['ref_1'] AND $r4['ref_1_label']=='return_item_id') {
-												$return_item_id = $r4['ref_1'];
-											} else if ($r4['ref_2'] AND $r4['ref_2_label']=='return_item_id') {
-												$return_item_id = $r4['ref_2'];
-											}
-
-											if ($return_item_id) {
-												$profitid = setCogs($inventoryid, $repair_item_id, 'repair_item_id', $cogs);
-											}
-										}
-									} else {
-										// for billable repairs, set cost of goods directly against repair item
-										$profitid = setCogs($inventoryid, $repair_item_id, 'repair_item_id', $cogs);
+								/***** GET TERMS FOR THIS REPAIR ORDER TO DETERMINE ITS TYPE - RMA OR REPAIR *****/
+								$NONBILLABLE = true;
+								$query4 = "SELECT termsid FROM repair_orders ro WHERE ro.ro_number = '".res($ro_number)."'; ";
+								$result4 = qdb($query4) OR die(qe().'<BR>'.$query4);
+								// with an existing record, check the terms id for anything other than "N/A" (non-billable)
+								if (mysqli_num_rows($result4)>0) {
+									$r4 = mysqli_fetch_assoc($result4);
+									if ($r4['termsid']<>15) {//15==N/A
+										$NONBILLABLE = false;
 									}
 								}
-							} else if (($r2['ref_1'] AND $r2['ref_1_label']=='sales_item_id') OR ($r2['ref_2'] AND $r2['ref_2_label']=='sales_item_id'])) {
+
+								// get cost of repair order for this unit
+								$cogs = calcRepairCost($ro_number,$repair_item_id,$inventoryid,$NONBILLABLE);//get repair cost
+
+								// If billable (! $NONBILLABLE), the $cogs gets added to the repair itself; if NON-billable,
+								// the $cogs should be added to the *originating* repair, not this most direct
+								// repair, since this is the warranty repair for that original billable repair
+								if (! $NONBILLABLE) {
+									// for billable repairs, set cost of goods directly against repair item
+									$profitid = setCogs($inventoryid, $repair_item_id, 'repair_item_id', $cogs);
+									continue;
+								}
+
+								/***** NON-BILLABLE, GET RMA DATA TO PRODUCE ORIGINATING BILLABLE ORDER *****/
+								$query4 = "SELECT ref_1, ref_1_label, ref_2, ref_2_label FROM repair_items ";
+								$query4 .= "WHERE id = '".$repair_item_id."' AND (ref_1_label = 'return_item_id' OR ref_2_label = 'return_item_id'); ";
+								$result4 = qdb($query4) OR die(qe().'<BR>'.$query4);
+								if (mysqli_num_rows($result4)==0) {
+									// RMA doesn't exist, which is admittedly weird and awkward, but let's get out of here
+									continue;
+								}
+								$r4 = mysqli_fetch_assoc($result4);
+								// determine which ref has return item id
+								$return_item_id = 0;
+								if ($r4['ref_1'] AND $r4['ref_1_label']=='return_item_id') {
+									$return_item_id = $r4['ref_1'];
+								} else if ($r4['ref_2'] AND $r4['ref_2_label']=='return_item_id') {
+									$return_item_id = $r4['ref_2'];
+								}
+
+								// look up the RMA to determine if the originating billable order was a Sale, or Repair
+								$query4 = "SELECT order_number, order_type FROM return_items ri, returns r ";
+								$query4 .= "WHERE ri.id = '".res($return_item_id)."' AND ri.rma_number = r.rma_number; ";
+								$result4 = qdb($query4) OR die(qe().'<BR>'.$query4);
+								if (mysqli_num_rows($result4)==0) {
+									// oops, missing record
+									continue;
+								}
+								$r4 = mysqli_fetch_assoc($result4);
+
+								if ($r4['order_type']=='Sale') {
+									// get the sales item id to which we attach the cogs
+									$query5 = "SELECT si.id FROM sales_items si, inventory_history h ";
+									$query5 .= "WHERE si.so_number = '".res($r4['order_number'])."' AND h.invid = '".res($inventoryid)."' ";
+									$query5 .= "AND si.id = h.value AND h.field_changed = 'sales_item_id'; ";
+									$result5 = qdb($query5) OR die(qe().'<BR>'.$query5);
+									if (mysqli_num_rows($result5)==0) { continue; }
+									$r5 = mysqli_fetch_assoc($result5);
+
+									$profitid = setCogs($inventoryid, $r5['id'], 'sales_item_id', $cogs);
+								} else if ($r4['order_type']=='Repair') {
+									$query5 = "SELECT ri.id FROM repair_items ri WHERE ri.ro_number = '".res($r4['order_number'])."' AND ri.invid = '".res($inventoryid)."'; ";
+									$result5 = qdb($query5) OR die(qe().'<BR>'.$query5);
+									if (mysqli_num_rows($result5)==0) { continue; }
+									$r5 = mysqli_fetch_assoc($result5);
+
+									$profitid = setCogs($inventoryid, $r5['id'], 'repair_item_id', $cogs);
+								}
+							} else if (($r2['ref_1'] AND $r2['ref_1_label']=='sales_item_id') OR ($r2['ref_2'] AND $r2['ref_2_label']=='sales_item_id')) {
 								/***** RMA REPLACEMENT *****/
 								// this is a replacement unit for a previous sale; get the avg cost of the replacement unit and apply it
 								// towards the cogs of the original sale
 								$cogs = getCost($partid);//get existing avg cost at this point in time
 
 								// We're adding the $cogs to the originating sales item id, which is what the ref1/ref2 label refers to
-								if ($r2['ref_1_label']=='sales_item_id']) {
-									$profitid = setCogs($inventoryid, $r2['ref_1'], $cogs);
+								if ($r2['ref_1_label']=='sales_item_id') {
+									$profitid = setCogs($inventoryid, $r2['ref_1'], 'sales_item_id', $cogs);
 								} else {/*$r2['ref_2_label']=='sales_item_id']*/
-									$profitid = setCogs($inventoryid, $r2['ref_2'], $cogs);
+									$profitid = setCogs($inventoryid, $r2['ref_2'], 'sales_item_id', $cogs);
+								}
+							} else if (($r2['ref_1'] AND $r2['ref_1_label']=='purchase_item_id') OR ($r2['ref_2'] AND $r2['ref_2_label']=='purchase_item_id')) {
+								/***** RTV *****/
+								$purchase_item_id = 0;
+								if ($r2['ref_1_label']=='purchase_item_id') {
+									$purchase_item_id = $r2['ref_1'];
+								} else {/*$r2['ref_2_label']=='purchase_item_id']*/
+									$purchase_item_id = $r2['ref_2'];
+								}
+/*DAVID, finish this out!
+								// if this item is being shipped back to vendor, the ref labels will have the originating purchase item id,
+								// so we treat it as a credit. find out if it was used for a sale, and walk the cogs back
+								$cogs = 0;
+								$query3 = "SELECT cogs_avg FROM sales_cogs WHERE inventoryid = '".res($inventoryid)."' AND item_id_label = 'sales_item_id' AND cogs_avg > 0; ";
+								$result3 = qdb($query3) OR die(qe().'<BR>'.$query3);
+								while ($r3 = mysqli_fetch_assoc($result3)) {
+									$cogs += $r3['cogs_avg'];
 								}
 
-								// if we're getting credit on the returned unit, credit back cogs from the original shipment
-
+								$cogs = getCost($partid);//get existing avg cost at this point in time
+								$profitid = setCogs($inventoryid, $r2['id'], 'sales_item_id', $cogs);
+*/
 							} else {
 								/***** SALE ITEM *****/
 								// this item is a billable sale and very straightforward; $r2['id'] is the sales item id
 								$cogs = getCost($partid);//get existing avg cost at this point in time
-								$profitid = setCogs($inventoryid, $r2['id'], $cogs);
+								$profitid = setCogs($inventoryid, $r2['id'], 'sales_item_id', $cogs);
 							}
 						}
 					}
