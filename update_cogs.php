fa9443be83fa2bdf873880e11317c3616ccbc9a6
David Langley <david@thewaitlessapp.com>
2017-07-03 14:53:21 -0700

    mocked up a script to update COGS journal entries

--- update_cogs.php
+++ update_cogs.php
@@ -1,8 +1,11 @@
 <?php
-	$query = "SELECT sc.* FROM sales_cogs sc, commissions c ";
+	include_once $_SERVER["ROOT_DIR"].'/inc/dbconnect.php';
+	include_once $_SERVER["ROOT_DIR"].'/inc/setJournalEntry.php';
+
+	$query = "SELECT sc.*, c.invoice_no FROM sales_cogs sc, commissions c ";
 	$query .= "WHERE c.invoice_item_id IS NOT NULL AND c.cogsid = sc.id AND c.commission_rate IS NOT NULL; ";
 	$result = qdb($query) OR die(qe().'<BR>'.$query);
 	while ($r = mysqli_fetch_assoc($result)) {
-		echo $r;
+		$jeid = setJournalEntry(false,$GLOBALS['now'],'Inventory Sale COGS','Inventory Asset','COGS for Invoice '.
 	}
 ?>
