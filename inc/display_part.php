75fc24daa2a67d365e847769c768e3d79f86c2c6
Andrew Kuan <andrew@ven-tel.com>
2017-07-19 16:49:56 -0700

    Auto stash before merge of "master" and "origin/master"

--- inc/display_part.php
+++ inc/display_part.php
@@ -6,9 +6,13 @@
         if($partid){
             $parts = current(hecidb($parts,"id"));
         }
-        $name = "<span class = 'descr-label'>".$parts['part']." &nbsp; ".$parts['heci']."</span>";
-        if($desc){
-            $name .= '<div class="description desc_second_line descr-label" style = "color:#aaa;">'.dictionary($parts['manf'])." &nbsp; ".dictionary($parts['system']).'</span> <span class="description-label">'.dictionary($parts['description']).'</span></div>';
+
+        //If hecidb fails to pull out the needed material then output nothing
+        if($parts['part']) {
+            $name = "<span class = 'descr-label'>".$parts['part']." &nbsp; ".$parts['heci']."</span>";
+            if($desc){
+                $name .= '<div class="description desc_second_line descr-label" style = "color:#aaa;">'.dictionary($parts['manf'])." &nbsp; ".dictionary($parts['system']).'</span> <span class="description-label">'.dictionary($parts['description']).'</span></div>';
+            }
         }
         return $name;
     }
--- shipping_report.php
+++ shipping_report.php
@@ -3,6 +3,7 @@
 	
 	include_once $rootdir.'/inc/dbconnect.php';
 	include_once $rootdir.'/inc/keywords.php';
+	include_once $rootdir.'/inc/filter.php';
 	include_once $rootdir.'/inc/display_part.php';
 	include_once $rootdir.'/inc/format_date.php';
 	include_once $rootdir.'/inc/format_price.php';
@@ -17,10 +18,16 @@
 	
 	$itemList = array();
 
+	if(!$companyid)
+		$companyid = setCompany();//uses $_REQUEST['companyid'] if passed in
+
+	$start_date = $_REQUEST['START_DATE'];
+	$end_date = $_REQUEST['END_DATE'];
+
+	//echo $start_date . ' ' . $end_date;
+
 	//Query Sales items that also contains repair items
-	$query = "SELECT datetime, companyid, partid, ref_1, ref_1_label, ref_2, ref_2_label, delivery_date, si.so_number as order_number, created, order_type, tracking_no, cust_ref FROM packages p, sales_items si, sales_orders so WHERE order_type = 'Sale' AND  p.order_number = si.so_number AND so.so_number = p.order_number AND si.price > 0 AND tracking_no IS NOT NULL
-		
-		ORDER BY created DESC;";
+	$query = "SELECT datetime, companyid, partid, ref_1, ref_1_label, ref_2, ref_2_label, delivery_date, si.so_number as order_number, created, order_type, tracking_no, cust_ref FROM packages p, sales_items si, sales_orders so WHERE order_type = 'Sale' AND  p.order_number = si.so_number AND so.so_number = p.order_number AND si.price > 0 ".($companyid ? ' AND companyid = "' .$companyid. '"': '')." ".dFilter('created', $start_date, $end_date)." ORDER BY created DESC;";
 		// UNION
 		// SELECT datetime, companyid, partid, ref_1, ref_1_label, ref_2, ref_2_label, receive_date as delivery_date, pi.po_number as order_number, created, order_type, tracking_no FROM packages p, purchase_items pi, purchase_orders po WHERE order_type = 'Purchase' AND  p.order_number = pi.po_number AND po.po_number = p.order_number AND pi.price > 0 
 	$result = qdb($query) OR die(qe());
@@ -42,7 +49,7 @@
 	while ($row = $result->fetch_assoc()) {
 		$itemList[] = $row;
 	}
-	
+
 ?>
 
 <!------------------------------------------------------------------------------------------->
@@ -70,26 +77,66 @@
 	</style>
 </head>
 
-<body class="sub-nav accounts-body">
-	
+<body class="sub-nav">
 	<?php include 'inc/navbar.php'; ?>
-	<div class="table-header" id = 'filter_bar' style="width: 100%; min-height: 48px;">
-		<div class="row" style="padding: 8px;" id = "filterBar">
-			<div class="col-md-4">
-				<div class="row">
-				
+
+	<form id="filter_form" method="POST">
+		<div class="table-header" id = 'filter_bar' style="width: 100%; min-height: 48px;">
+			<div class="row" style="padding: 8px;" id = "filterBar">
+				<div class="col-md-4">
+				    <div class="btn-group medium col-sm-6 remove-pad" data-toggle="buttons">
+				        <button data-toggle="tooltip" data-placement="right" title="" data-original-title="Sales" class="btn btn-sm left filter_status btn-default" data-filter="sale">
+				        	Sales	
+				        </button>
+				        <button data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Repair" class="btn btn-sm middle filter_status btn-default" data-filter="repair">
+				        	Repair	
+				        </button>
+						<button data-toggle="tooltip" data-placement="bottom" title="" data-original-title="All" class="btn btn-sm right filter_status active btn-info" data-filter="all">
+				        	All
+				        </button>
+				    </div>
+
+					<div class="col-sm-3 remove-pad">
+						<div class="input-group datepicker-date date datetime-picker" data-format="MM/DD/YYYY" data-maxdate="07/19/2017" data-hposition="right">
+				            <input type="text" name="START_DATE" class="form-control input-sm" value="<?=($start_date ? $start_date : '')?>" style="min-width:50px;">
+				            <span class="input-group-addon">
+				                <span class="fa fa-calendar"></span>
+				            </span>
+				        </div>
+					</div>
+					<div class="col-sm-3 remove-pad">
+						<div class="input-group datepicker-date date datetime-picker" data-format="MM/DD/YYYY" data-maxdate="07/19/2017" data-hposition="right">
+				            <input type="text" name="END_DATE" class="form-control input-sm" value="<?=($end_date ? $end_date : '07/19/2017');?>" style="min-width:50px;">
+				            <span class="input-group-addon">
+				                <span class="fa fa-calendar"></span>
+				            </span>
+				    	</div>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-md-4 text-center remove-pad">
-            	<h2 class="minimal" id="filter-title">Shipping Report</h2>
-			</div>
-			
-			<div class="col-md-4 remove-pad">
-			</div>
+				<div class="col-md-4 text-center remove-pad">
+	            	<h2 class="minimal" id="filter-title">Shipping Report</h2>
+				</div>
+				
+				<div class="col-md-4">
+					<div class="pull-right form-group" style="margin-bottom: 0;">
+						<select name="companyid" id="companyid" class="company-selector">
+							<option value="">- Select a Company -</option>
+							<?php 
+								if ($companyid) { 
+									echo '<option value="'.$companyid.'" selected>'.getCompany($companyid).'</option>'.chr(10); 
+								} else { 
+									echo '<option value="">- Select a Company -</option>'.chr(10); 
+								} 
+							?>
+						</select>
+						<input class="btn btn-primary btn-sm" type="submit" value="Go">
+					</div>
+				</div>
 
+			</div>
 		</div>
-	</div>
+	</form>
 	<div id="pad-wrapper">
 		<div class="row">
 			<table class="table heighthover heightstriped table-condensed p_table">
@@ -102,35 +149,38 @@
 						<th class="col-md-1">Due Date</th>
 						<th class="col-md-1">Shipped</th>
 						<th class="col-md-2">Tracking#</th>
-
-					
 					</tr>
 				</thead>
 				<tbody>
-					<?php foreach($itemList as $part): ?>
-						<tr>
+					<?php 
+						foreach($itemList as $part): 
+							$order = 0;
+							$order_det = '';
+							$type = 'sale_item';
+									
+							if($part['ref_1_label'] == 'repair_item_id') {
+								$order = getRepairOrder($part['ref_1']);
+								$order_det = "Repair# " . $order;
+								$type = 'repair_item';
+							} else if($part['ref_2_label'] == 'repair_item_id') {
+								$order = getRepairOrder($part['ref_2']);
+								$order_det = "Repair# " . $order;
+								$type = 'repair_item';
+							} else {
+								$order = $part['order_number'];
+								$order_det = $part['order_type']."# " . $order;
+							}
+					?>
+						<tr class="<?=$type;?> filter_item">
 							<td><?= getCompany($part['companyid']); ?></td>
 							<td>
 								<?php 
-									$order;
-									
-									if($part['ref_1_label'] == 'repair_item_id') {
-										$order = getRepairOrder($part['ref_1']);
-										echo "Repair# " . $order;
-									} else if($part['ref_2_label'] == 'repair_item_id') {
-										$order = getRepairOrder($part['ref_2']);
-										echo "Repair# " . $order;
-									} else {
-										$order = $part['order_number'];
-										echo $part['order_type']."# " . $order;
-									}
+									echo $order_det;
 								?>	
 							</td>
 							<td>
 								<?php
-									
-										echo $part['cust_ref'];
-									//} 
+									echo $part['cust_ref'];
 								?>
 							</td>
 							<td><?=display_part($part['partid'], true); ?></td>
@@ -149,8 +199,45 @@
 	</div>
 
 	<?php include_once 'inc/footer.php'; ?>
+	<!-- <script src="js/operations.js?id=<?php if (isset($V)) { echo $V; } ?>"></script> -->
 
     <script type="text/javascript">
+
+    	(function($){
+
+    		$(document).on("click onload", ".filter_status", function(e){
+    			var type = $(this).data('filter');
+				$('.filter_item').hide();
+				$('.filter_status').removeClass('active');
+				$('.filter_status').removeClass('btn-warning');
+				$('.filter_status').removeClass('btn-success');
+				$('.filter_status').removeClass('btn-info');
+				$('.filter_status').removeClass('btn-flat info');
+				$('.filter_status').removeClass('btn-flat gray');
+				$('.filter_status').removeClass('btn-flat danger');
+				$('.filter_status').removeClass('btn-danger');
+				$('.filter_status').addClass('btn-default');
+
+				var btn,type2;
+				if (type=='repair') {
+					btn = 'success';
+					type2 = type;
+					$('.repair_item').hide();
+				} else if (type=='sale') {
+					btn = 'warning';
+					type2 = type;
+					$('.sale_item').hide();
+				} else {
+					type = 'all';
+					type2 = 'filter';
+					btn = 'info';
+				}
+
+				$('.filter_status[data-filter="'+type+'"]').addClass('btn-'+btn);
+				$('.'+type2+'_item').show();
+    		});
+
+		})(jQuery);
     </script>
 
 </body>
