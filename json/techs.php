7e00337d2f2735a09eecc7b15b40781e26c61fb2
Andrew Kuan <andrew@ven-tel.com>
2017-10-03 17:19:32 -0700

    Add In Service Completion Pages

---
+++ json/techs.php
@@ -0,0 +1,24 @@
+<?php
+	include_once '../inc/dbconnect.php';
+
+	// Service Type : FFR, repair, etc
+	$type = strtolower($_REQUEST['type']);
+    
+    //array('id'=>$id,'text'=>$text));
+    $output = array();
+    
+    $query = "SELECT u.id as userid, c.name 
+    			FROM service_classes sc, user_classes uc, users u, contacts c 
+    			WHERE LOWER(sc.class_name) = '".res($type)."' AND uc.classid = sc.id AND u.id = uc.userid AND c.id = u.contactid;";
+
+    $result = qdb($query) OR die(qe() . ' ' . $query);
+    
+    while($r = mysqli_fetch_assoc($result)) {
+        $output[] = array(
+            'id' => $r['userid'], 
+            'text' => $r['name']
+        );
+    }
+    
+	echo json_encode($output);
+	exit;
---
+++ modal/service_complete.php
@@ -0,0 +1,58 @@
+<?php
+  $repair_codes = array();
+  $query = "SELECT * FROM repair_codes;";
+
+  $result = qdb($query) or die(qe() . ' ' . $query);
+
+  while ($row = $result->fetch_assoc()) {
+    $repair_codes[] = $row;
+  }
+?>
+<div class="modal modal-alert fade" id="modal-complete" tabindex="-1" role="dialog" aria-labelledby="modalAlertTitle">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span>&times;</span></button>
+        <h4 class="modal-title" id="modalAlertTitle">Complete Order</h4>
+	  </div>
+	  
+	   <form action="repair_activities.php" method="post">
+	      <div class="modal-body" id="modalAlertBody">
+			<div class="row">
+				<?php if(empty($serials)) { ?>
+					<div id="alert_message" class="alert alert-danger fade in text-center alert-ship" style="width: 100%; z-index: 9999; top: 95px;">
+						<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>
+						<strong id="alert_title">Error</strong>: No Item(s) have been scanned for this order! 
+					</div>
+				<?php } ?>
+			</div><!-- row -->
+	
+			<div class="row">
+
+				<input type="text" name="ro_number" value="<?=$order_number;?>" class="hidden">
+				<input type="text" name="techid" value="<?=$U['id'];?>" class="hidden">
+
+				<div class="col-md-12">
+					<select class="form-control" name="repair_code">
+						<option selected="" value="null">- Select Status -</option>
+						<?php 
+							foreach($repair_codes as $code):
+								echo "<option value='".$code['id']."'>".$code['description']."\t".$code['code']."</option>";
+							endforeach;
+						?>
+					</select>
+				</div>
+			</div><!-- row -->
+			<div class="row">
+				<div class="col-md-12">
+				</div>
+			</div><!-- row -->
+		</div>
+	      <div class="modal-footer text-center">
+	   		<button type="button" class="btn btn-default btn-sm btn-dismiss" data-dismiss="modal">Cancel</button>
+	        <button class="btn-sm btn btn-success pull-right btn-update" type="submit" name="type" value="complete_ticket"><i class="fa fa-save"></i> Complete Ticket</button>
+		  </div>
+		</div>
+	</form>
+  </div>
+</div>
