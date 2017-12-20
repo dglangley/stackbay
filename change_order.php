377caaa8ab02b2da71980336b6b03108d3c2feb1
David Langley <david@thewaitlessapp.com>
2017-12-19 17:04:06 -0800

    CO: task name increment for CO's

--- change_order.php
+++ change_order.php
@@ -24,19 +24,21 @@
 
 	$T = order_type($order_type);
 
-	$max_ln = 1;
+	$taskid = 0;
+	$max_ln = 0;
 	$query = "SELECT * FROM ".$T['items']." WHERE ".$T['order']." = '".res($order_number)."'; ";
 	$result = qedb($query);
 	if (mysqli_num_rows($result)==0) {
 		die('Uh oh: '.$query);
 	}
 	while ($r = mysqli_fetch_assoc($result)) {
-		if ($r['id']==$line_item_id) { $items = $r; }
-
-		// get max LN for adding line below
-//12/15/17
-//		if ($r['line_number']>$max_ln) { $max_ln = $r['line_number']; }
+		if ($r['id']==$line_item_id) {
+			$items = $r;
+		} else if ($r['ref_2_label']==$T['item_label'] AND $r['ref_2']) {
+			if ($r['task_name']>$max_ln) { $max_ln = $r['task_name']; }
+		}
 	}
+	$max_ln++;
 
 	// user has decided for a new line item
 	if ($new_item) {
@@ -56,10 +58,12 @@
 			else if ($item=='ref_2_label') { $val = $T['item_label']; }
 			else if ($item=='qty') { $val = 1; }
 			else if ($item==$T['description']) { $val = $description; }
+			else if ($item=='task_name') { $val = $max_ln; }
 			$val_query .= fres($val);
 		}
 		$query .= $item_query.") VALUES (".$val_query."); ";
 		$result = qedb($query);
+		$taskid = qid();
 	} else if ($new_order) {
 		// insert new order
 	}
@@ -67,6 +71,10 @@
 	if ($DEBUG) { exit; }
 
 	//header('Location: edit_order.php?order_type='.$order_type.'&order_number='.$order_number);
-	header('Location: service.php?order_type='.$order_type.'&order_number='.$order_number.'-'.$items['line_number']);
+	if ($taskid) {
+		header('Location: service.php?order_type='.$order_type.'&taskid='.$taskid);
+	} else {
+		header('Location: service.php?order_type='.$order_type.'&order_number='.$order_number.'-'.$items['line_number']);
+	}
 	exit;
 ?>
