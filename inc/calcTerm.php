cc8a6e64f99c30e679642b3c26e45097b2f64b98
Andrew Kuan <andrew@ven-tel.com>
2018-01-25 11:47:40 -0800

    Push Aaronless calcTerm function

--- inc/calcTerm.php
+++ inc/calcTerm.php
@@ -5,24 +5,21 @@ include_once $_SERVER['ROOT_DIR'] .'/inc/format_date.php';
 include_once $_SERVER['ROOT_DIR'] .'/inc/order_type.php';
 
 	function calcTerm($order_number, $order_type){
+		$dueDate = '';
+
 		$T = order_type($order_type);
-		// $o = o_params($order_type);
-		// $due_select = "
-		// SELECT created, days FROM ".$o['order'].", terms WHERE termsid = terms.id and ".$o['id']." = ".prep($order_number).";";
-		// $due_estimate_result = qdb($due_select) or die(qe()." | $due_select");
-		// $due_estimate_arr = mysqli_fetch_assoc($due_estimate_result);
-		// if($due_estimate_arr['days'] > 0){
-		// 	$due_estimate = format_date($due_estimate_arr['created'], "n/j/Y", array("d"=>$due_estimate_arr['days']));
-		// } else {
-		// 	$due_estimate = format_date($due_estimate_arr['created'], "n/j/Y");
-		// }
+
+		$query = " SELECT ".$T['datetime']." as created, days FROM ".$T['orders'].", terms WHERE termsid = terms.id and ".$T['order']." = ".fres($order_number).";";
+		$result = qedb($query);
+
+		if(mysqli_num_rows($result)) {
+			$r = mysqli_fetch_assoc($result);
+			if($r['days'] > 0){
+				$dueDate = format_date($r['created'], "n/j/Y", array("d"=>$r['days']));
+			} else {
+				$dueDate = format_date($r['created'], "n/j/Y");
+			}
+		}
 
 		return($dueDate);
 	}
-	
-	// Only Used in an import script to killing it (/auto/import/import_component_bills.php)
-	// function getDays($id){
-	// 	return rsrq("SELECT `days` FROM `terms` where `id` = ".prep($id).";");
-	// }
-
-?>
