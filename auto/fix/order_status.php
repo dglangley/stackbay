179721ed01903bd2d2d3b3b61a9cee92b8c66828
Andrew Kuan <andrew@ven-tel.com>
2018-03-07 16:29:53 -0800

    Added Number formatting to the charges and credits

--- auto/fix/order_status.php
+++ auto/fix/order_status.php
@@ -15,17 +15,20 @@ $DEBUG = 0;
 		$query2 = "SELECT po_number FROM purchase_items WHERE po_number = ".res($r['po_number'])." AND qty_received < qty;";
 		$result2 = qedb($query2);
 
+		$charges = number_format(getOrderCharges($r['po_number'], $T));
+		$credits = number_format(getOrderCredits($r['po_number'], $T));
+
 		echo 'Purchase: ' . $r['po_number'].' '.$r['status'].'<BR>';
-		echo 'Charges: '.getOrderCharges($r['po_number'], $T).' Credits: '.getOrderCredits($r['po_number'], $T) . '<BR>';
+		echo 'Charges: '.$charges.' Credits: '.$credits . '<BR>';
 
 		// If there are no results of the received qty being lower than the qty order than the order is complete
 		if (mysqli_num_rows($result2) == 0) { $status = 'Complete'; } else { $status = 'Active'; }
 
-		if (getOrderCharges($r['po_number'], $T) <= getOrderCredits($r['po_number'], $T) AND $status == 'Complete') { 
+		if ($charges <= $credits AND $status != 'Active') { 
 			echo '<b>Changing Status to Closed</b><BR><BR>' ;
 			$status = 'Closed';
 		} else { 
-			echo '<b>Status Unchanged</b><BR><BR>';
+			echo '<b>Status Unchanged: </b>'.$status.'<BR><BR>';
 		}
 
 		if ($status<>$r['status']) {
@@ -44,17 +47,20 @@ $DEBUG = 0;
 		$query2 = "SELECT so_number FROM sales_items WHERE so_number = ".res($r['so_number'])." AND qty_shipped < qty;";
 		$result2 = qedb($query2);
 
+		$charges = number_format(getOrderCharges($r['so_number'], $T));
+		$credits = number_format(getOrderCredits($r['so_number'], $T));
+
 		echo 'Sale: ' . $r['so_number'].' '.$r['status'].'<BR>';
-		echo 'Charges: '.getOrderCharges($r['so_number'], $T).' Credits: '.getOrderCredits($r['so_number'], $T) . '<BR>';
+		echo 'Charges: '.$charges.' Credits: '.$credits . '<BR>';
 
 		// If there are no results of the shipped qty being lower than the qty order than the order is complete
 		if (mysqli_num_rows($result2) == 0) { $status = 'Complete'; } else { $status = 'Active'; }
 
-		if (getOrderCharges($r['so_number'], $T) <= getOrderCredits($r['so_number'], $T) AND $status == 'Complete') { 
+		if ($charges <= $credits AND $status == 'Complete') { 
 			echo '<b>Changing Status to Closed</b><BR><BR>' ;
 			$status = 'Closed';
 		} else { 
-			echo '<b>Status Unchanged</b><BR><BR>';
+			echo '<b>Status Unchanged: '.$status.'</b><BR><BR>';
 		}
 
 		if ($status<>$r['status']) {
@@ -74,13 +80,16 @@ $DEBUG = 0;
 		$query2 = "SELECT ro_number FROM repair_items WHERE ro_number = ".res($r['ro_number'])." AND repair_code_id IS NULL;";
 		$result2 = qedb($query2);
 
+		$charges = number_format(getOrderCharges($r['ro_number'], $T));
+		$credits = number_format(getOrderCredits($r['ro_number'], $T));
+
 		echo 'Repair: ' . $r['ro_number'].' '.$r['status'].'<BR>';
-		echo 'Charges: '.getOrderCharges($r['ro_number'], $T).' Credits: '.getOrderCredits($r['ro_number'], $T) . '<BR>';
+		echo 'Charges: '.$charges.' Credits: '.$credits . '<BR>';
 
 		// If there is no repair line items in the order that does not have a null repair code
 		if (mysqli_num_rows($result2) == 0) { $status = 'Complete'; } else { $status = 'Active'; }
 
-		if (getOrderCharges($r['ro_number'], $T) <= getOrderCredits($r['ro_number'], $T) AND $status == 'Complete') { 
+		if ($charges <= $credits AND $status == 'Complete') { 
 			echo '<b>Changing Status to Closed</b><BR><BR>' ;
 			$status = 'Closed';
 		} else { 
@@ -104,13 +113,16 @@ $DEBUG = 0;
 		$query2 = "SELECT so_number FROM service_items WHERE so_number = ".res($r['so_number'])." AND status_code IS NULL;";
 		$result2 = qedb($query2);
 
+		$charges = number_format(getOrderCharges($r['so_number'], $T));
+		$credits = number_format(getOrderCredits($r['so_number'], $T));
+
 		echo 'Service: ' . $r['so_number'].' '.$r['status'].'<BR>';
-		echo 'Charges: '.getOrderCharges($r['so_number'], $T).' Credits: '.getOrderCredits($r['so_number'], $T) . '<BR>';
+		echo 'Charges: '.$charges.' Credits: '.$credits . '<BR>';
 
 		// If there is no status line items in the order that does not have a null status code
 		if (mysqli_num_rows($result2) == 0) { $status = 'Complete'; } else { $status = 'Active'; }
 
-		if (getOrderCharges($r['so_number'], $T) <= getOrderCredits($r['so_number'], $T) AND $status == 'Complete') { 
+		if ($charges <= $credits AND $status == 'Complete') { 
 			echo '<b>Changing Status to Closed</b><BR><BR>' ;
 			$status = 'Closed';
 		} else { 
@@ -134,13 +146,16 @@ $DEBUG = 0;
 		$query2 = "SELECT rma_number FROM return_items, inventory_history ih WHERE rma_number = ".res($r['rma_number'])." AND ih.field_changed = 'returns_item_id' AND value = return_items.id AND ih.value IS NULL;";
 		$result2 = qedb($query2);
 
+		$charges = number_format(getOrderCharges($r['rma_number'], $T));
+		$credits = number_format(getOrderCredits($r['rma_number'], $T));
+
 		echo 'Return: ' . $r['rma_number'].' '.$r['status'].'<BR>';
-		echo 'Charges: '.getOrderCharges($r['rma_number'], $T).' Credits: '.getOrderCredits($r['rma_number'], $T) . '<BR>';
+		echo 'Charges: '.$charges.' Credits: '.$credits . '<BR>';
 
 		// If there is no status line items in the order that does not have a null status code
 		if (mysqli_num_rows($result2) == 0) { $status = 'Complete'; } else { $status = 'Active'; }
 
-		if (getOrderCharges($r['rma_number'], $T) <= getOrderCredits($r['rma_number'], $T) AND $status == 'Complete') { 
+		if ($charges <= $credits AND $status == 'Complete') { 
 			echo '<b>Changing Status to Closed</b><BR><BR>' ;
 			$status = 'Closed';
 		} else { 
