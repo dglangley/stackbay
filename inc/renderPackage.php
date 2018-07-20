d0afedd4a8e2fbba4117ea3e84136e2329f68a4c
Andrew Kuan <andrew@ven-tel.com>
2018-07-20 13:32:00 -0700

    Fix issue with packing slip

--- inc/renderPackage.php
+++ inc/renderPackage.php
@@ -225,7 +225,7 @@ foreach($packageids as $packageid) {
 	$html_page_str .='
 	        <div id = "letter_head">
 	            <b>
-	                <img src="'.$PROFILE['logo'].'" style="width:1in;"></img><br>
+	                <img src="https://www.stackbay.com/'.$PROFILE['logo'].'" style="width:1in;"></img><br>
 	                '.(address_out(getCompanyAddressid($PROFILE['companyid']))?:getCompany($PROFILE['companyid'])).' <br>
 	                '.getCompany($PROFILE['companyid'], 'id', 'phone').'
 	            </b>
